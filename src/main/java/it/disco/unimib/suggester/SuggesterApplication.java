539ab5e2472bd1babba1502d58781fc62eb6c3a7
miciav <michele.ciavotta@gmail.com>
2019-07-10 16:47:38 +0200

    Initial commit

---
+++ src/main/java/it/disco/unimib/suggester/SuggesterApplication.java
@@ -0,0 +1,13 @@
+package it.disco.unimib.suggester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuggesterApplication.class, args);
+    }
+
+}
